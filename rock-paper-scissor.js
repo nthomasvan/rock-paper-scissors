bbd2edd2baad96c56b55a1e41d53014eb778fa2a
Thomas Nguyen <nthomasvan@gmail.com>
2023-01-16 22:00:30 +1100

    Initial commit of Rock paper scissors ui

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let computerChoice = getRandomInt(3);
 
@@ -21,6 +24,26 @@ function getRandomInt(max){
 }
 
 function playRound(playerSelection, computerSelection){
+    if(
+        (playerSelection === "rock" && computerSelection === "scissors") || 
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ){
+        playerScore++;
+    }
+    else if(
+        (playerSelection === "rock" && computerSelection === "paper") || 
+        (playerSelection === "paper" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "rock")
+    ){
+        computerScore++;
+    }
+    updateScore(playerSelection, computerSelection);
+
+    if(playerScore == 5 || computerScore == 5){
+        document.getElementById('result').innerHTML = "You Won!";
+    }
+    /*
     switch(playerSelection){
         case "rock":
             if(computerSelection == "rock")
@@ -64,11 +87,40 @@ function playRound(playerSelection, computerSelection){
                 return "You lose! Scissors cut paper!";
             }
             break;
-    }
+            */
 }
 
+function log(e){
+    console.log(this);
+}
+
+function processChoice(playerSelection){
+    const computerSelect = getComputerChoice();
+    playRound(playerSelection,computerSelect);
+}
+
+function updateScore(playerSelection,computerSelection){
+    document.getElementById('playerScore').innerHTML = `Player: ${playerScore}`
+    document.getElementById('computerScore').innerHTML = `Computer: ${computerScore}`
+    document.getElementById('playerChoice').innerHTML = `Player Chose: ${playerSelection}`
+    document.getElementById('computerChoice').innerHTML = `Computer Chose: ${computerSelection}`
+}
+
+const rockBtn = document.getElementById("rockBtn");
+const paperBtn = document.getElementById("paperBtn");
+const scissorsBtn = document.getElementById("scissorsBtn");
+
+
+rockBtn.addEventListener('click',() => processChoice('rock'));
+paperBtn.addEventListener('click',() => processChoice('paper'));
+scissorsBtn.addEventListener('click',() => processChoice('scissors'));
+
+
+/*
 let answer = prompt("Rock Paper Scissors! Please type in choice:");
 var test = getComputerChoice();
 console.log("computer choice: " + test);
 console.log("Player choice: " + answer);
 console.log(playRound(answer,test));
+
+*/
